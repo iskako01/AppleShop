79e8c62b42dea41de04882bf62864d9f33a8dfdf
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-07-11 18:33:04 +0200

    register api

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const register = require("./routes/register");
 
 const products = require("./products");
 
 const app = express();
 
+require("dotenv").config();
+
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/register", register);
 
 app.get("/", (req, res) => {
   res.send("Welcome our to online shop API...");
@@ -17,5 +23,14 @@ app.get("/products", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
+const url = process.env.DB_URL;
 
 app.listen(port, console.log(`Server running on port ${port}`));
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection succsesful..."))
+  .catch((err) => console.log("MongoDB connection failed", err.message));
---
+++ backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 30 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minLength: 6, maxLength: 1024 },
+});
+
+const User = mongoose.model("User", userSchema);
+
+exports.User = User;
